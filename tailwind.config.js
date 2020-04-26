faac4811329c97f1c27a929b09942542b8ff0fb8
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-04-26 23:22:50 +0700

    Add tailwindcss with config and sass dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const { colors } = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  theme: {
+    extend: {},
+    colors: {
+      ...colors,
+      primary: '#3498DB',
+      secondary: '#F1C40F',
+      neutral: '#34495E',
+      success: '#2ECC71',
+      danger: '#E74C3C'
+    }
+  },
+  variants: {},
+  plugins: []
+}
